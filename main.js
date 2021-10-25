2d7a05758da6caa6b5f6594229d2c103f802449f
Tristan Schmale <tristanschmale@Tristans-iMac.local>
2021-10-25 14:36:15 +0000

    saving update

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // progression bar function
 window.onload = move = () =>  {
     const elem = document.getElementById("myBar");  
+    const text = document.getElementById("text"); 
     let height = 0; // starting 
     const id = setInterval(frame, 25); // progression speed
     
@@ -10,7 +11,11 @@ window.onload = move = () =>  {
       } else {
         height++; 
         elem.style.height = height + '%';
-        elem.innerHTML = height * 1  + '%';
+        // elem.innerHTML = height * 1  + '%';
+        text.innerHTML = height * 1  + '%';
+        text.style.color = 'black';
+        text.style.padding = '1rem';
+        text.style.fontWeight = 'bold';
       }
     }
   }
